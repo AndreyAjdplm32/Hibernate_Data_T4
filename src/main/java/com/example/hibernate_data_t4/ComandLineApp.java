b29cefbcc03242855d5e2784c9df6e632569b43a
AndreyAndrianov jdplm-32 <darkc0lder@mail.ru>
2023-09-07 23:26:39 +0300

    4

--- src/main/java/com/example/hibernate_data_t4/ComandLineApp.java
+++ src/main/java/com/example/hibernate_data_t4/ComandLineApp.java
@@ -1,24 +1,14 @@
 package com.example.hibernate_data_t4;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @SpringBootApplication
-public class ComandLineApp implements CommandLineRunner {
+public class ComandLineApp {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(ComandLineApp.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
-
+        SpringApplication.run(ComandLineApp.class,args);
+    }
     }
 
-    @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-
-    }
-}
